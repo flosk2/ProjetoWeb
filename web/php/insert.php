e3aabbca5032584c21a35fbc47300707c23651ff
Marcos André <kaytotrabs@gmail.com>
2017-05-11 21:20:20 -0300

    Add files via upload

---
+++ web/php/insert.php
@@ -0,0 +1,20 @@
+<?php
+include 'db.php';
+
+$user = $_POST['login'];
+$senha = $_POST['senha'];
+
+$sql = "insert INTO usuarios (usuario, senha) values ('$user','$senha')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+
+
+
+
+?>
