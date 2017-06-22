257c948c5a8f2470de5448dc9a331e31b3d548d6
Marcos André <kaytotrabs@gmail.com>
2017-06-22 08:34:39 -0300

    Update ConsultasMarcadas.php

--- site/php/ConsultasMarcadas.php
+++ site/php/ConsultasMarcadas.php
@@ -1,42 +1,113 @@
 <?php
 include "db.php";
 session_start();
-
-
-
 //$cidade = "Taguatinga";
 //$especi = "Nutricionista";
-
 //$cidade = $_POST['a'];
 //$especi = $_POST['b'];
 $id = $_SESSION['id'];
-
 $sql = "SELECT * from consultas_usuario where usuario = '$id';";
 $result = $conn->query($sql);
 if ($result->num_rows > 0) {
     // output data of each row
     
 	?>
+
 <!DOCTYPE HTML>
 <html>
 <head>
 <meta charset="utf-8" /> 
 <title>Minha Consulta</title>
-
+<link href="css/bootstrap.css" rel='stylesheet' type='text/css' />
+<link href="css/style.css" rel='stylesheet' type='text/css' />
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <link href='http://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,800' rel='stylesheet' type='text/css'>
-<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+
+	  
+<style type="text/css">
+table {
+font-size:12px;
+color:#ffffff;
+width:97%;
+border-width: 1px;
+border-color: #4180c5;
+margin: 0 auto;
+}
+.tftable th 
+{font-size:12px;
+background-color:#696969;
+border-width: 1px;
+padding: 8px;
+border-style: solid;
+border-color: #4180c5;
+text-align:center;
+border-radius: 5px;
+font-size: 120%;
+}
+table tr {
+background-color:#9cbfc1;
+font-size: 120%;
+font-family: "arial";
+font-size:15px
+}
+table td {
+font-size:12px;
+border-width: 1px;
+padding: 8px;
+border-color: #4180c5;
+border-radius: 5px;
+font-size: 120%;
+}
+table tr:hover {
+background-color:#728889;
+cursor:  pointer; 
+}
+table th:hover {
+cursor: default; 
+}
+input[type=text]{
+	margin-bottom: 20px;
+	margin-top: 10px;
+	width:75%;
+	padding: 15px;
+	border-radius: 5px;
+	border:1px solid #7ac9b7;
+}
+.painel{
+ margin-left: auto;
+ margin-right: auto;
+ opacity: 0.9;
+}
+.tabela{
+    margin-bottom: 20px;
+	margin-top: 10px;
+    
+	width:100%;
+	border-radius:5px;
+	border:1px solid #7ac9b7;
+	background:#99cfd8;
+	color: #00b711;
+    text-align: center;
+    
+    font-family: "arial";
+    
+     
+    
+}
+.painel .tabela .text{
+text-align: center;
+}
+html,body{
+background-size:contain;
+background-repeat: no-repeat;
+background-size: 100% 100%; 
+}
+</style>
 <script src="js/jquery-1.9.1.min.js"></script>
-
-
-
-
-
 <!--hover-effect-->
 <script src="js/hover_pack.js"></script>
 <script type="text/javascript" src="js/jquery.mixitup.min.js"></script>
-
 	<script type="text/javascript">
 	$(function () {
 		
@@ -72,33 +143,24 @@ if ($result->num_rows > 0) {
 				);				
 			
 			}
-
 		};
 		
 		// Run the show!
 		filterList.init();
 		
 		
-		
-		
 	});	
 	</script>
 </head>
 <body>
-	
-	
-	
-	
-	
-	
 	<!--start header-->
 	<div class="header">
 	  <div class="header-top">
 		<div class="container">
 			<div class="logo">
-			  <a href=<?php echo $servidor."/index.php"?>><img src="images/logo.png" alt=""/></a>
+			  <a href="http://igorlisboa.esy.es/index.php"><img src="images/logo.png" alt=""/></a>
 			</div>
-				
+									
 	        <div class="clear"></div>
 	        <script type="text/javascript" src="js/responsive-nav.js"></script>
 			<script type="text/javascript" src="js/move-top.js"></script>
@@ -114,120 +176,6 @@ if ($result->num_rows > 0) {
 		   </div>
 		 </div>
 		 </div>
-		 <section class="primeiroEspaço">
-		 <div class="header-bottom" id="home">
-		  <div class="primeiraLista">
-
-	  
-<style type="text/css">
-
-
-
-
-table {
-font-size:12px;
-color:#ffffff;
-width:97%;
-border-width: 1px;
-border-color: #4180c5;
-margin: 0 auto;
-}
-
-
-
-.tftable th 
-{font-size:12px;
-background-color:#696969;
-border-width: 1px;
-padding: 8px;
-border-style: solid;
-border-color: #4180c5;
-text-align:center;
-border-radius: 5px;
-font-size: 120%;
-
-}
-table tr {
-background-color:#9cbfc1;
-font-size: 120%;
-font-family: "arial";
-font-size:15px
-
-}
-
-
-table td {
-font-size:12px;
-border-width: 1px;
-padding: 8px;
-border-color: #4180c5;
-border-radius: 5px;
-font-size: 120%;
-
-
-}
-table tr:hover {
-background-color:#728889;
-cursor:  pointer; 
-
-
-}
-table th:hover {
-cursor: default; 
-
-
-
-}
-
-input[type=text]{
-	margin-bottom: 20px;
-	margin-top: 10px;
-	width:75%;
-	padding: 15px;
-	border-radius: 5px;
-	border:1px solid #7ac9b7;
-}
-
-.painel{
- margin-left: auto;
- margin-right: auto;
- opacity: 0.9;
-
-}
-
-.tabela{
-    margin-bottom: 20px;
-	margin-top: 10px;
-    
-	width:100%;
-	border-radius:5px;
-	border:1px solid #7ac9b7;
-	background:#99cfd8;
-	color: #00b711;
-    text-align: center;
-    
-    font-family: "arial";
-    
-     
-    
-}
-.painel .tabela .text{
-text-align: center;
-
-}
-
-
-html,body{
-
-
-
-background-size:contain;
-background-repeat: no-repeat;
-
-background-size: 100% 100%; 
-
-}
-</style>
 
 <div class="painel">
 <div class="tabela">
@@ -248,7 +196,6 @@ while($row = $result->fetch_assoc()) {
 } else {
    header("Location: semConsultasMarcadas.html");
 }
-
 ?>	
 
 </table>
